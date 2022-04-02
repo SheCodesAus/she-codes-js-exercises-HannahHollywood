421adb7e2844574952c395dbfd06a9efb66abbeb
HannahHollywood <70626629+HannahHollywood@users.noreply.github.com>
2022-04-02 10:47:57 +0800

    Added features to Shopping List

--- playground-starter/script.js
+++ playground-starter/script.js
@@ -1,6 +1,41 @@
-console.log("Hello, world!");
+// console.log("Hello, world!");
 
 function showName() {
     let username = document.getElementById("name-text").value;
     console.log(username);
+}
+
+// Lists & Arrays //
+
+// for (let count = 0; count < 10; count++) {
+//     console.log(count);
+// }
+
+// let pets = ["Chilli", "Rex", "Roary", "Fleur"]
+// console.log(pets);
+
+// pets.push("Ivy");
+// console.log(pets);
+
+// console.log(pets[0]);
+// console.log(pets[3]);
+// console.log(pets.length);
+
+// for (let index = 0; index < pets.length; index++) {
+//     console.log(pets[index]);
+// }
+
+// pets.forEach((pet) => {
+//     console.log(pet);
+// });
+
+
+let pet = "Chilli";
+
+if (pet == "Chilli") {
+    console.log("OMG you are the cutest!");
+} else if (pet == "Ivy") {
+    console.log("Naaaaw cute bow.");
+} else {
+    console.log("OMG so cute");
 }
--- shopping-list-starter/script.js
+++ shopping-list-starter/script.js
@@ -1,3 +1,28 @@
-function addItem() {}
+let shoppingItems = ["milk", "eggs", "bread"]
 
-function updateItems() {}
+function updateItems() {
+    let listElement = document.getElementById("shopping-list-items");
+    listElement.innerHTML = "";
+    shoppingItems.forEach((item) => {
+        let itemElement = document.createElement("li");
+        itemElement.innerText = item
+        listElement.appendChild(itemElement);
+});
+}
+updateItems()
+
+function addItem() {
+    let item = document.getElementById("new-item-text").value;
+    shoppingItems.push(item);
+    console.log(shoppingItems);
+    updateItems()
+    // Hide previously entered text (reset the input)
+    document.getElementById("new-item-text").value = "";
+}
+
+function clearList() {
+    shoppingItems = [];
+    // shoppingItems.length=0;
+    updateItems()
+}
+
